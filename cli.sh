781481c244c962f24d56400d3ea4a19ac282be92
Christian Kaisermann <christian@kaisermann.me>
2020-04-22 14:54:55 -0300

    fix: 🐛 fix again cli dangerfile path

--- cli.sh
+++ cli.sh
@@ -2,4 +2,4 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 PWD=$(pwd)
 
-$PWD/node_modules/.bin/danger $1 -d $SCRIPTPATH/action/dangerfile.js "${@:2}"
+$PWD/node_modules/.bin/danger $1 -d $PWD/node_modules/@vtex/danger/action/dangerfile.js "${@:2}"
